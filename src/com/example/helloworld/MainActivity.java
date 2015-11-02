f149e775fa66a3ff6699f92def199c74a58c977e
mvaishnavi <vaishnavi4@gmail.com>
2015-11-02 22:04:40 +0530

    Hello World  App

---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.helloworld;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text= (TextView) findViewById(R.id.text_view_id);
+        String store =text.getText().toString();
+        text.setText(store);
+        int n = store.length();
+        text.setText("No. of letters:"+Integer.toString(n));
+        
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
